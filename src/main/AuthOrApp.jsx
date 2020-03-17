b1aee34a0a6438d82d2bf1e63651407b2e3bade8
unknown <henriquehps1997@gmail.com>
2020-03-17 14:05:37 -0300

    ajustes no layout para o unidos adtag

--- src/main/AuthOrApp.jsx
+++ src/main/AuthOrApp.jsx
@@ -10,17 +10,11 @@ class AuthOrApp extends Component{
     
     render(){
 
-        //
-        const url_string = window.location.href;
-
-        //
-        const url = new URL(url_string);
-
         //Pega token da url
-        const token_tmp = url.searchParams.get('key');
+        const token_tmp = ''
         
         //Pega user da url 
-        const name_user = url.searchParams.get('user');
+        const name_user = '';
 
         //Valida se o token foi passado na url
         const validateToken = token_tmp ? sessionStorage.setItem('token', JSON.stringify({ token: token_tmp })) : token_tmp
@@ -28,23 +22,17 @@ class AuthOrApp extends Component{
         //Busca o token na session storage
         const token = sessionStorage.getItem('token')
 
-        //Limpa a url
-        if(token_tmp || name_user){
-            window.history.pushState("/", document.title, window.location.pathname);
-        }
-        
-        if(validateToken || token){
+        // if(validateToken || token){
             
-            const validateUser = name_user ? sessionStorage.setItem('user', JSON.stringify({ user: name_user })) : ''
+        //     const validateUser = name_user ? sessionStorage.setItem('user', JSON.stringify({ user: name_user })) : ''
             
-            return(<App />)
+        //     return(<App />)
             
-        }else{
+        // }else{
             
-            // return(<Auth />)
-            return window.location.href = 'http://desenv-gonline.iesb.br'
+            return(<Auth />)
 
-        }
+        // }
 
 
     }
