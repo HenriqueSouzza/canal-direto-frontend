5284a0deedd0083832083e3d231ddf67d910d317
unknown <henriquehps1997@gmail.com>
2020-05-07 17:43:16 -0300

    ajuste no processo de inscricao

--- src/pages/acampUnidos/inscrever/passoDois.jsx
+++ src/pages/acampUnidos/inscrever/passoDois.jsx
@@ -11,7 +11,16 @@ class PassoDois extends Component{
     render(){
         return(
             <div className="text-left">
+                <h4>Dados do evento</h4>
+                <div className="row justify-content-center">
+                    <div className="col-md-5">sdsdssds</div>
+                </div>
+                <hr/>
                 <h4>Forma de pagamento</h4>
+                    <div className="row justify-content-center">
+                        <div className="col-md-5">sdsds</div>
+                    </div>
+                <hr/>
                 <button className="btn btn-danger" onClick={() => this.props.onClickPasso({passoAtual: '1'})}>Voltar</button>
                 <button className="btn btn-success" onClick={() => this.props.onClickPasso({passoAtual: '3'})}>Proximo</button>
             </div>
