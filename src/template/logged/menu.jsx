234ab4123db8a8c2b2557c331d41410082d34593
unknown <henriquehps1997@gmail.com>
2020-08-20 12:37:11 -0300

    correcao e adição de menus

--- src/template/logged/menu.jsx
+++ src/template/logged/menu.jsx
@@ -10,18 +10,21 @@ function Menu(){
         <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
                 {/* <MenuLink description={`Dashboard`} path={`/`} icon={`fas fa-tachometer-alt`}  active={``} /> */}
-                <MenuTreeView description={`Dados cadastrais`} path={`#`} icon={`fas fa-user`} >
+                {/* <MenuTreeView description={`Dados cadastrais`} path={`#`} icon={`fas fa-user`} >
                     <MenuLink description={`Meus dados`} path={`/dados-cadastrais/meus-dados`} icon={`fa fa-user-edit`} active={``} />
                     <MenuLink description={`Alterar Senha`} path={`/dados-cadastrais/alterar-senha`} icon={`fa fa-key`} active={``} />
-                </MenuTreeView>
+                </MenuTreeView> */}
                 <MenuTreeView description={`Meus ticket`} path={`#`} icon={`fa fa-address-card`} >
                     <MenuLink description={`Fechados`} path={`/tickets/meus-tickets`} icon={`fa fa-lock`} active={``} />
                     <MenuLink description={`Abertos`} path={`/tickets/meus-tickets`} icon={`fa fa-unlock`} active={``} />
                 </MenuTreeView>
-                <MenuTreeView description={`Ticket meu DEPTO`} path={`#`} icon={`fa fa-clone`} >
-                    <MenuLink description={`Fechados`} path={`/tickets/meus-tickets`} icon={`fa fa-lock`} active={``} />
-                    <MenuLink description={`Abertos`} path={`/tickets/meus-tickets`} icon={`fa fa-unlock`} active={``} />
+                <MenuTreeView description={`Tickets meu setor`} path={`#`} icon={`fa fa-clone`} >
+                    <MenuLink description={`Para meu setor`} path={`/tickets/meus-tickets`} icon={`fa fa-lock`} active={``} />
                     <MenuLink description={`Meus tickets`} path={`/tickets/meus-tickets`} icon={`fa fa-address-card`} active={``} />
+                </MenuTreeView>
+                <MenuTreeView description={`Gerenciar tickets`} path={`#`} icon={`fa fa-clone`} >
+                    <MenuLink description={`Do meu setor`} path={`/tickets/meus-tickets`} icon={`fa fa-unlock`} active={``} />
+                    {/* <MenuLink description={`Para meu setor`} path={`/tickets/meus-tickets`} icon={`fa fa-unlock`} active={``} /> */}
                 </MenuTreeView>
                 <MenuLink description={`Setor`} path={`/setor`} icon={`fa fa-users`} active={``} />
                 <MenuLink description={`Sair`} path={`/sair`} icon={`fa fa-sign-out-alt`} active={``} />
