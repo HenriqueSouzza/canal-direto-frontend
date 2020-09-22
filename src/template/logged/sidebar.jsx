28d4a6822a10217945e64005973ec41dfa243ab7
unknown <henriquehps1997@gmail.com>
2020-09-22 14:23:02 -0300

    ajustes nas telas de login

--- src/template/logged/sidebar.jsx
+++ src/template/logged/sidebar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+import { bindActionCreators } from 'redux';
+
 import { Link } from 'react-router-dom';
 
 import Menu from './menu';
@@ -10,7 +14,8 @@ import imgLogo  from '../images/logo.png';
 
 import { USER_LOGGED } from '../../config/const';
  
-function Sidebar() {
+function Sidebar(props) {
+
     return (
         <aside className="main-sidebar sidebar-dark-light elevation-4">
             <Link to={`#`} className="brand-link navbar-danger text-sm">
@@ -23,13 +28,25 @@ function Sidebar() {
                         <img src={imgPerfil} className="img-circle elevation-2" alt={`UserImage`} />
                     </div>
                     <div className="info">
-                        <Link to={`#`} className="d-block">{ USER_LOGGED.usuario ? USER_LOGGED.usuario : 'Sem nome' }</Link>
+                        <Link to={`#`} className="d-block">{ props.auth.user.email ? props.auth.user.email : 'Sem nome' }</Link>
                     </div>
                 </div>
                 <Menu />
             </div>
         </aside>
     )
+
 }
 
-export default Sidebar;
+/**
+ * @param {*} state 
+ */
+const mapStateToProps = state => ({ auth: state.auth })
+
+/**
+ * @param {*} dispatch 
+ */
+const mapDispatchToProps = dispatch => bindActionCreators({ }, dispatch);
+
+
+export default connect(mapStateToProps, mapDispatchToProps )(Sidebar);
