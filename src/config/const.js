440d20a1ea0cb5e280296dd0f7f87f3d0c8cc0a5
unknown <henriquehps1997@gmail.com>
2020-10-27 10:35:40 -0300

    ajuste no host dos endpoints

--- src/config/const.js
+++ src/config/const.js
@@ -1,7 +1,7 @@
 
 export const BASE_API = (
     process.env.NODE_ENV === 'production' ? 
-        'http://desenv-apl.iesb.br/sistemas-academicos-api/'
+        'https://desenv-apl.iesb.br/sistemas-academicos-api/'
     :
     process.env.NODE_ENV === 'development' ?
         'http://sistemas-academicos.desenv.br/sistemas-academicos-api/public/'
