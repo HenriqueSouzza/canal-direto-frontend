7bf84b676a635ecea9c99841e98a64175b282dc4
unknown <henriquehps1997@gmail.com>
2020-11-05 06:56:03 -0300

    implementacao e ajustes na tela de atendimento

--- src/pages/ticketsSetor/paraMeuSetor/index.jsx
+++ src/pages/ticketsSetor/paraMeuSetor/index.jsx
@@ -79,12 +79,12 @@ class Index extends Component{
                 setor: row.setor,
                 indicador:  row.dt_interacao ? 
                                 moment().subtract(3, "days") >= moment(row.dt_interacao) ? 'text-danger'
-                                : moment().subtract(2, "days") >= moment(row.dt_interacao) ? 'text-warning'
+                                : moment().subtract(2, "days") >= moment(row.dt_interacao) ? 'text-orange'
                                 : moment().subtract(1, "days") >= moment(row.dt_interacao) ? 'text-yellow'
                                 : 'text-success'
                             :
                                 moment().subtract(3, "days") >= moment(row.dt_criacao) ? 'text-danger'
-                                : moment().subtract(2, "days") >= moment(row.dt_criacao) ? 'text-warning'
+                                : moment().subtract(2, "days") >= moment(row.dt_criacao) ? 'text-orange'
                                 : moment().subtract(1, "days") >= moment(row.dt_criacao) ? 'text-yellow'
                                 : 'text-success',
                 categoria: row.categoria,
